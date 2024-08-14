69266282263db5cb32f0d69b97f5681f4204984a
Iordan Ionut <mariusionut879@yahoo.com>
2024-08-14 15:45:18 +0300

    init

--- frontend/App_Core/src/app/app.component.ts
+++ frontend/App_Core/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInputModule, MatFormFieldModule, MatFormField],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'App_Core';
