ad87b58ab2eae9f69d9dd20d601420b95ac140f3
Iordan Ionut <mariusionut879@yahoo.com>
2024-08-14 15:23:59 +0300

    init

---
+++ frontend/App_Core/src/app/core/components/line/line.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@Component({
+  selector: 'app-line',
+  standalone: true,
+  imports: [BrowserAnimationsModule],
+  templateUrl: './line.component.html',
+  styleUrl: './line.component.scss',
+})
+export class LineComponent {}
